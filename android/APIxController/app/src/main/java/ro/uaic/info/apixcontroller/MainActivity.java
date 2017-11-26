e7dcedbddb191d0949f6811c6554f4af140daa0e
alin-corodescu <alin.corodescu@gmail.com>
2017-11-26 15:38:48 +0200

    added dummy js function call

--- android/APIxController/app/src/main/java/ro/uaic/info/apixcontroller/MainActivity.java
+++ android/APIxController/app/src/main/java/ro/uaic/info/apixcontroller/MainActivity.java
@@ -2,6 +2,7 @@ package ro.uaic.info.apixcontroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView = (WebView) findViewById(R.id.main_web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl("file:///android_asset/www/index.html");
     }
 }
