d11872a319da367b71052afdec0a31adad7da5d9
alin-corodescu <alin.corodescu@gmail.com>
2017-11-26 22:17:39 +0200

    implemented the interface for the android app

--- android/APIxController/app/src/main/assets/www/javascript/app.js
+++ android/APIxController/app/src/main/assets/www/javascript/app.js
@@ -1,3 +1,16 @@
+
 function sayHello() {
     alert("sayHello function called");
+}
+
+function clickColorPicker() {
+	document.getElementById("colorPicker").click();
+}
+
+function clickCamera() {
+	document.getElementById("camera").click();
+}
+function updateColorPickerButton() {
+	var color = document.getElementById("colorPicker").value;
+	document.getElementById("colorButton").style.backgroundColor = color;
 }
--- android/APIxController/app/src/main/java/ro/uaic/info/apixcontroller/MainActivity.java
+++ android/APIxController/app/src/main/java/ro/uaic/info/apixcontroller/MainActivity.java
@@ -1,11 +1,28 @@
 package ro.uaic.info.apixcontroller;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PICKFILE_REQUEST_CODE = 1;
+    private ValueCallback<Uri[]> mFilePathCallback;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICKFILE_REQUEST_CODE) {
+            Uri result = data == null || resultCode == RESULT_OK ? null : data.getData();
+            Uri[] resultsArray = new Uri[1];
+            resultsArray[0] = result;
+            if (result != null)
+                mFilePathCallback.onReceiveValue(resultsArray);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
         WebView webView = (WebView) findViewById(R.id.main_web_view);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                Log.d("FileChooser", "showFileChooser called");
+                mFilePathCallback = filePathCallback;
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, PICKFILE_REQUEST_CODE);
+                return true;
+            }
+        });
         webView.loadUrl("file:///android_asset/www/index.html");
     }
 }
