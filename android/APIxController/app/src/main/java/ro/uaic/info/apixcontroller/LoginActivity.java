9421c7e13e424c607e71b074bdbf80a9f0bc5b41
alin-corodescu <alin.corodescu@gmail.com>
2017-11-28 23:55:19 +0200

    implemented login activity without transitions

---
+++ android/APIxController/app/src/main/java/ro/uaic/info/apixcontroller/LoginActivity.java
@@ -0,0 +1,23 @@
+package ro.uaic.info.apixcontroller;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        WebView webView = (WebView) findViewById(R.id.login_web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/www/startpage/startpage.html");
+    }
+}
