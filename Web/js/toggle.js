adb0316ee63ab4f443f36c2d1d9d239244f77ad2
alin-corodescu <alin.corodescu@gmail.com>
2017-11-29 08:35:54 +0000

    implemented navbar

--- Web/js/toggle.js
+++ Web/js/toggle.js
@@ -10,3 +10,33 @@ function toggle() {
         x.style.visibility = "hidden";
     }
 }
+function changeOpacityOfContainers(y) {
+	var x = document.getElementsByClassName("container");
+	var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.opacity = "" + y;
+    }
+}
+function displayLinks(flag) {
+	var x = document.getElementsByClassName("container");
+	var i;
+    for (i = 0; i < x.length; i++) {
+    	if (flag == true)
+        	x[i].style.visibility = "visible";
+        else x[i].style.visible ="hidden";
+    }
+}
+function closeNavigation() {
+	document.getElementById("sidenav").style.width = "0";
+	// document.body.style.opacity = "1";	
+	// document.getElementById("sidenav").style.opacity = "0";
+	changeOpacityOfContainers(1);
+	displayLinks(false);
+}
+
+function openNavigation() {
+	document.getElementById("sidenav").style.width = "20%";
+	document.getElementById("sidenav").style.opacity = "1 !important";
+	changeOpacityOfContainers(0.2);
+	displayLinks(true);
+}
