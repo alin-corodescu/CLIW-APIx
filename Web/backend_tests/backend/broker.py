6d9f05c1a1057d7f7174d44c0d12462a7ad0186b
alin-corodescu <alin.corodescu@gmail.com>
2018-01-03 00:45:35 +0200

    implemented publish subscribe protocol with different sessions an initiation method

--- Web/backend_tests/backend/broker.py
+++ Web/backend_tests/backend/broker.py
@@ -1,29 +1,16 @@
-import sys
-from flask import Flask
+
 import asyncio
 
 from BrokeringProtocol import BrokeringProtocol
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
 if __name__ == '__main__':
-    # websockets.serve(hello_world, 'localhost', 5000)
-
-    # log.startLogging(sys.stdout)
-
     from autobahn.asyncio.websocket import WebSocketServerFactory
 
     factory = WebSocketServerFactory()
     factory.protocol = BrokeringProtocol
 
     loop = asyncio.get_event_loop()
-    coro = loop.create_server(factory, '127.0.0.1', 5000)
+    coro = loop.create_server(factory, host=None, port=5000)
     server = loop.run_until_complete(coro)
 
     try:
@@ -33,4 +20,3 @@ if __name__ == '__main__':
     finally:
         server.close()
         loop.close()
-    # app.run()
