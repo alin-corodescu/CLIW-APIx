4003f4cd09de14b66c22b4e7b6c1d200a96ffe37
alin-corodescu <alin.corodescu@gmail.com>
2018-01-06 23:30:22 +0000

    implemented test for session id

--- Web/backend_tests/backend/BrokeringProtocol.py
+++ Web/backend_tests/backend/BrokeringProtocol.py
@@ -1,16 +1,24 @@
 from autobahn.asyncio.websocket import WebSocketServerProtocol
 # or: from autobahn.asyncio.websocket import WebSocketServerProtocol
+from selenium_test import Session
+
+MESSAGE_Q_LEN_LIMIT = 1000
 class Broker(object):
     def __init__(self):
         self.publishers = {}
         self.subscribers = {}
         self.topics = {}
+        self.sessions = {}
 
-    def register(self, client, session):
+    def register(self, client, session, width, height):
         self.publishers.update({client: session})
         if self.subscribers.get(session) == None:
             self.subscribers[session] = []
         self.subscribers[session].append(client)
+        if session not in self.sessions:
+            # Start a new session
+            self.sessions[session] = Session(sessionID = session, width = width, height = height)
+
 
 
     def publishMessage(self, client, payload):
@@ -18,6 +26,7 @@ class Broker(object):
         if self.topics.get(topic) == None:
             self.topics[topic] = []
         self.topics[topic].append(payload)
+        self.handleMessageQueue(topic)
         self.broadcastUpdates(topic)
 
     def broadcastUpdates(self, topic):
@@ -26,6 +35,7 @@ class Broker(object):
             subscriber.sendMessage(self.topics[topic][-1], False)
 
     def initiate(self, client):
+        # TODO send the canvas state as an update
         session = self.publishers[client]
         if self.topics.get(session) != None:
             for message in self.topics[session]:
@@ -42,6 +52,15 @@ class Broker(object):
         # Remove the client from this topic
         clients.remove(client)
 
+    def handleMessageQueue(self, topic):
+        if len(self.topics[topic]) > MESSAGE_Q_LEN_LIMIT:
+            sess = self.sessions.get(topic)
+            # Store the states for the canvases
+            sess.store_state_for_canvases(['drawable_canvas', 'background_canvas'])
+            # clear the messages for the topic
+            self.topics[topic].clear()
+
+
 
 broker = Broker()
 
@@ -49,7 +68,7 @@ class BrokeringProtocol(WebSocketServerProtocol):
 
     def onConnect(self, request):
         print("Client connecting: {}".format(request.peer))
-        broker.register(self, request.params['sessionID'][0])
+        broker.register(self, request.params['sessionID'][0], request.params['width'][0], request.params['height'][0])
 
     def onOpen(self):
         print("WebSocket connection open.")
