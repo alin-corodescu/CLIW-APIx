20f234f6a162561a1cbeb48c172c72ad3a31f9be
CristiHuma96 <cristihuma96@gmail.com>
2018-01-07 19:54:49 +0200

    Finished integrating zoom and scroll effect + did some refactorization of the code

--- Web/js/canvas.js
+++ Web/js/canvas.js
@@ -1,3 +1,5 @@
+// Variables required throughout the file
+
 // The url to connect to the backend for synchronization
 const BACKEND_URL = 'ws://localhost:5000/';
 
@@ -10,8 +12,6 @@ var zoom_type = {
     IN: 1,
     OUT: 2
 };
-
-var scale = 1.0, scaleStep = 0.25, scale_type = null, zoomPointX = null, zoomPointY = null;
 
 // Returns the position of an element in the page
 function computePosition(element) {
@@ -29,6 +29,23 @@ function computePosition(element) {
         return [ element.x, element.y ];
     }
 }
+
+function setInitialSettings(mouse_active, mouseData, mode, visorState, currentStyle){
+    mouse_active = false;
+    // Mode in which to use the mouse e.g scroll or paint
+    mode = modes.PAINT;
+
+    mouseData = {xTo: 0, xFrom: 0, yTo: 0 , yFrom: 0};
+
+    // Change this object through the web interface
+    visorState = {zoom : 1, zoomStep: 0.1, offsetX: 0, offsetY: 0};
+
+    // Here we specify the color, the thickness etc.
+    currentStyle = {color : "black", thickness : 2};
+
+    return [mouse_active, mouseData, mode, visorState, currentStyle];
+}
+
 // This is the main function, will be called when the browser loads the page
 var main = function () {
 
@@ -52,6 +69,8 @@ var main = function () {
 	var background_canvas = document.getElementById('background_canvas');
 	var drawable_canvas = document.getElementById('drawable_canvas');
 	var switch_button = document.getElementById('switch');
+    let colorPicker = document.getElementById("colorPicker");
+    let lineWeight = document.getElementById("lineWeight");
 
 	// This settings here have to be done because canvas CSS width and height do not get propagated
     // to the actual context, it's two different values
@@ -63,28 +82,14 @@ var main = function () {
     drawable_canvas.width = parseInt(window.getComputedStyle(drawable_canvas).width);
     drawable_canvas.height = parseInt(window.getComputedStyle(drawable_canvas).height);
 
-
 	var drawable_canvas_ctx = drawable_canvas.getContext('2d');
 	var backgroud_canvas_ctx = background_canvas.getContext('2d');
 	var visor_ctx = visor.getContext('2d');
 
-	var mouse_active = false;
-	// Mode in which to use the mouse e.g scroll or paint
-	var mode = modes.PAINT;
+	var mouse_active, mouseData, mode, visorState, currentStyle;
+    let scale_type = null, zoomPointX = null, zoomPointY = null;
 
-    // Switch between modes
-    switch_button.onclick = function (ev) {
-        mode = modes.PAINT + modes.SCROLL - mode;
-        // console.log("mode is " + mode)
-        };
-
-	var mouseData = {xTo: 0, xFrom: 0, yTo: 0 , yFrom: 0};
-
-    // Change this object through the web interface
-    var visorState = {zoom : 1, zoomStep: 0.1, offsetX: 0, offsetY: 0};
-
-    // Here we specify the color, the thickness etc.
-    var currentStyle = {color : "black", thickness : 2};
+    [mouse_active, mouseData, mode, visorState, currentStyle] = setInitialSettings()
 
     // Transforms mouse coordinates into actual canvas coordinates using the current
     // visor state (e.g. zoom, offsets etc)
@@ -137,7 +142,14 @@ var main = function () {
         mouseData.yFrom = mouseData.yTo;
 	};
 
-	visor.onmousedown = function(event) {
+    //Adding event handlers
+
+    // Switch between modes
+    switch_button.onclick = function (ev) {
+        mode = modes.PAINT + modes.SCROLL - mode;
+    };
+
+    visor.onmousedown = function(event) {
 		mouse_active = true;
 		[mouseData.xFrom, mouseData.yFrom] = computeActualMousePosition(event);
 	};
@@ -170,11 +182,16 @@ var main = function () {
         }
     };
 
-    var dummy_canvas = document.createElement('canvas');
-    dummy_canvas.style.border = "2px solid black";
-    dummy_canvas.width = visor.width;
-    dummy_canvas.height = visor.height;
-    var dummy_canvas_ctx = dummy_canvas.getContext('2d');
+    colorPicker.onchange = function(){
+        currentStyle.color = colorPicker.value;
+    };
+
+    //Creating a transfer canvas to palce the zoomed image in it then redraw it on the visor canvas
+    let transfer_canvas = document.createElement('canvas');
+    transfer_canvas.width = visor.width;
+    transfer_canvas.height = visor.height;
+    let transfer_canvas_ctx = transfer_canvas.getContext('2d');
+
 
     function renderVisor() {
         let image, image2 = new Image(visor.width, visor.height);
@@ -192,17 +209,17 @@ var main = function () {
 
         // Saving the image from the drawable canvas : getImageData -> dummy_canvas -> toDataURL -> drawImage
         image = drawable_canvas_ctx.getImageData(visorState.offsetX, visorState.offsetY, visor.width / visorState.zoom, visor.height/visorState.zoom );
-        dummy_canvas_ctx.putImageData(image, 0, 0);
+        transfer_canvas_ctx.putImageData(image, 0, 0);
 
         image2.onload=function(){
-            dummy_canvas_ctx.clearRect(0, 0, dummy_canvas.width, dummy_canvas.height);
+            transfer_canvas_ctx.clearRect(0, 0, transfer_canvas.width, transfer_canvas.height);
             visor_ctx.save();
             visor_ctx.clearRect(0,0,visor.width,visor.height);
             visor_ctx.scale(visorState.zoom,visorState.zoom);
             visor_ctx.drawImage(image2,0,0);
             visor_ctx.restore();
         };
-        image2.src = dummy_canvas.toDataURL();
+        image2.src = transfer_canvas.toDataURL();
 
     }
 
