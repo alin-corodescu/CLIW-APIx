bbefc6532d4be76e8fc10aed7192e894027bc4b2
CristiHuma96 <cristihuma96@gmail.com>
2018-01-15 22:15:33 +0200

    Added responsiveness and initial modal for settings -- works all, just need to update it faster on resize

--- Website/js/modal.js
+++ Website/js/modal.js
@@ -4,7 +4,7 @@ function initModal(){
     let modal_background = document.getElementById('modal_background');
     let modal_connection = document.getElementById('modal_connection');
     let modal_shareable_link = document.getElementById('modal_shareable_link');
-    let modal_initial_settings = document.getElementById('modal_initial_settings');
+    // let modal_initial_settings = document.getElementById('modal_initial_settings');
 
     // Get the button that opens the modal/modals
     // add custom event if u want to fire on specific action
@@ -14,7 +14,7 @@ function initModal(){
     let modal_background_hide = document.getElementById("modal_background_hide");
     let modal_connection_hide = document.getElementById("modal_connection_hide");
     let modal_shareable_link_hide = document.getElementById("modal_shareable_link_hide");
-    let modal_initial_settings_hide = document.getElementById('modal_initial_settings_hide');
+    // let modal_initial_settings_hide = document.getElementById('modal_initial_settings_hide');
 
     // When the user clicks the button, open the modal/modals
     // modal_show.onclick = function() {
@@ -31,9 +31,9 @@ function initModal(){
     modal_shareable_link_hide.onclick = function() {
         modal_shareable_link.style.display = "none";
     };
-    modal_initial_settings_hide.onclick = function(){
-        modal_initial_settings.style.display = "none";
-    }
+    // modal_initial_settings_hide.onclick = function(){
+    //     modal_initial_settings.style.display = "none";
+    // }
 
     // When the user clicks anywhere outside of the modal//modals, close it
     window.onclick = function(event) {
